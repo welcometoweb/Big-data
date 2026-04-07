f73e898a8df79734718e97115790f6f1c353ac10
welcometoweb <62826293+welcometoweb@users.noreply.github.com>
2026-04-08 00:06:26 +0530

    Create lambda_function.py

---
+++ aws_worklow/lambda_function.py
@@ -0,0 +1,12 @@
+import json
+
+def lambda_handler(event, context):
+    print("🔥 Lambda triggered successfully!")
+
+    print("📥 Event received:")
+    print(json.dumps(event, indent=2))
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("Lambda is working 🚀")
+    }
